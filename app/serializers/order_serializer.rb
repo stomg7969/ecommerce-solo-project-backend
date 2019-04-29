32996ddf8069283ff1b6191eba0644e0df87eab5
stomg7969 <stomg7969@gmail.com>
2019-04-29 18:16:12 -0400

    Added more attributes on serializer

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,5 +1,5 @@
 class OrderSerializer < ActiveModel::Serializer
-  attributes :id, :user, :total_amount, :shipping_method, :status, :created_at, :updated_at
+  attributes :id, :user, :details, :total_amount, :shipping_method, :status, :created_at, :updated_at
 
   def user
     {
@@ -7,5 +7,23 @@ class OrderSerializer < ActiveModel::Serializer
       'Ordered by' => self.object.user.username,
       :email => self.object.user.email
     }
+  end
+
+  def details
+    self.object.order_details.map do |detail|
+      {
+        id: detail.id,
+        order: detail.updated_at,
+        quantity: detail.quantity,
+        size: detail.size,
+        product: {
+          :id => detail.product.id,
+          :name => detail.product.name,
+          :price => detail.product.price,
+          :image => detail.product.imgBack,
+          :category => detail.product.category
+        }
+      }
+    end
   end
 end
