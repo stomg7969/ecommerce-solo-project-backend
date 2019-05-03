32117b9f0384807a48eb13a35c2fe7c39ab978cf
stomg7969 <stomg7969@gmail.com>
2019-05-03 17:19:50 -0400

    Added a comment for future reference

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,7 @@ class ProductsController < ApplicationController
 
   def index
     products = Product.all
-    render json: products
+    render json: products.sort_by { |product| product.id }
   end
 
   def create
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,12 @@
 class Product < ApplicationRecord
   has_many :order_details
   has_many :orders, :through => :order_details
+
+  # def self.find_by_materials(materials)
+  #   self.all.select do |prod|
+  #     # (prod.materials & materials).empty?
+  #     prod.materials.any? { |mat| materials.include?(mat) }
+  #   end
+  # end
+  # ONE WAY TO FILTER 
 end
